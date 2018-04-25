8ea3da45258a4f62543fd2248d69d4771d008998
daniel giron <38026432+qdgironq@users.noreply.github.com>
2018-04-25 15:00:29 -0500

    initial

---
+++ ThemeParkDatabase/Pages/Employees/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ThemeParkDatabase.Models;
+
+namespace ThemeParkDatabase.Pages.Employees
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ThemeParkDatabase.Models.ThemeParkDatabaseContext _context;
+
+        public CreateModel(ThemeParkDatabase.Models.ThemeParkDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentId"] = new SelectList(_context.Department, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Employee.Add(Employee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ThemeParkDatabase/ViewModels/AttractionsQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThemeParkDatabase.Models;
+
+namespace ThemeParkDatabase.ViewModels
+{
+    public class AttractionsQuery
+    {
+        public ReportQuery ReportQuery { get; set; }
+        public List<Attraction> Attractions { get; set; }
+    }
+}
